65ad5982db00faae21a32a40359f878d40a05021
Dieye-code <moustaphadieye96@gmail.com>
2022-03-26 01:17:10 +0000

    remove template

--- src/layouts/Base.js
+++ src/layouts/Base.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import RouterR from '../router/RouterR';
-import Aside from './Aside';
-import Footer from './Footer';
-import Header from './Header';
-import SideBar from './SideBar';
 
 const Base = () => {
   return <div>
     <div className="wrapper">
-      <Header />
       <RouterR />
-      <Aside />
-      <Footer />
     </div>
   </div>;
 };
--- src/router/RouterR.js
+++ src/router/RouterR.js
@@ -3,11 +3,9 @@ import React from 'react';
 import { Link, Router, Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from '../components/Home';
 import Accueil from '../components/Accueil';
-import SideBar from '../layouts/SideBar';
 
 const RouterR = () => {
     return <div>
-            <SideBar />
             <Routes>
                 <Route path="/" element={<Accueil />} />
                 <Route path="/home" element={<Home />} />
